dc76837e44c7eccccbbd02a3f51985dea6878bd8
alansalvaterra <alan.salvaterra@gmail.com>
2024-06-05 21:48:34 -0300

    feat: lógica para renderizar unidades por município

---
+++ frontend/src/app/services/municipio.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MunicipioService {
+
+  private apiUrl = 'http://localhost:3000/api/municipio';
+
+  constructor(private http: HttpClient) { }
+
+  getMunicipios(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
