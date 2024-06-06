066c4c1bf3fa70a02f749b04b366335f229d7571
alansalvaterra <alan.salvaterra@gmail.com>
2024-06-06 17:28:11 -0300

    style: personalizando header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { InputTextModule } from 'primeng/inputtext'; 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'; 
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -23,9 +21,7 @@ import { DropdownModule } from 'primeng/dropdown';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    InputTextModule,
     HttpClientModule,
-    FormsModule,
     ButtonModule,
     DropdownModule,
   ],
